56cdc8916ef00638eab8eb0e3cd50f62a31b3552
vlmna13 <vlmna13@gmail.com>
2024-11-08 15:11:59 +0300

    add sollutions for getLastDigit, parseNumberFromString

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -127,8 +127,8 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return value % 10;
 }
 
 /**
@@ -142,8 +142,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return +value;
 }
 
 /**
